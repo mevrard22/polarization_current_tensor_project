3ddd275ae87c783340ae995e45682ae5415f0942
Maxime Evrard <maximee2022@student.cis.edu.hk>
2022-06-23 16:53:03 -0700

    * Rename adjust value inputs * Give description to adjust_value * Imported numpy into functions.py * Imported sympy into functions.py and main.py * Make printing modified matrix come before calculating the dc current vector * Added dc current vector function (didn't check if it is actually working or not) * Commented out copy and paste to clipboard

--- functions.py
+++ functions.py
@@ -1,15 +1,45 @@
-def adjust_value(i_index, j_index, threeByThree, threeBySix):
+import numpy as np
+import sympy as sp  # Symbolic mathematics package
+
+# Adjust value will take in two indices (for the column and row of the 3x6 matrix)
+# , as well as a placeholder 3x3x3 matrix the 3x6 matrix which is to be converted
+# NOTE: these calculations currently only works for point groups where
+# jk → j if j=k, jk → 9-(j+k) if j≠k in the 3x6 matrix
+
+
+def adjust_value(i_index, j_index, three_by_three, three_by_six):
     # Handle cases where looking at first, second or third index of 3x6 matrix
     if j_index <= 2:
-        threeByThree[i_index][j_index][j_index] = threeBySix[i_index][j_index]
+        three_by_three[i_index][j_index][j_index] = three_by_six[i_index][j_index]
     # Individually handle cases for when index (j) is 4, 5 or 6
     # Change rules as necessary
     elif j_index == 3:
-        threeByThree[i_index][1][2] = threeBySix[i_index][j_index]
-        threeByThree[i_index][2][1] = threeBySix[i_index][j_index]
+        three_by_three[i_index][1][2] = three_by_six[i_index][j_index]
+        three_by_three[i_index][2][1] = three_by_six[i_index][j_index]
     elif j_index == 4:
-        threeByThree[i_index][0][2] = threeBySix[i_index][j_index]
-        threeByThree[i_index][2][0] = threeBySix[i_index][j_index]
+        three_by_three[i_index][0][2] = three_by_six[i_index][j_index]
+        three_by_three[i_index][2][0] = three_by_six[i_index][j_index]
     elif j_index == 5:
-        threeByThree[i_index][1][0] = threeBySix[i_index][j_index]
-        threeByThree[i_index][0][1] = threeBySix[i_index][j_index]
+        three_by_three[i_index][1][0] = three_by_six[i_index][j_index]
+        three_by_three[i_index][0][1] = three_by_six[i_index][j_index]
+
+# Get the matrix which has been converted
+# Define the list En
+# Perform double sum
+
+
+def calculate_dccurrent_vector(matrix):
+    # Defining expressions before I can use them
+    x = sp.symbols('x'); cosine = sp.cos(x); sine = sp.sin(x)
+    # Defining the components of the magnetic field and final vector, respectively
+    en = [cosine, sine, 1]
+    vector_component_list = [0, 0, 0]
+    # Working out what the final expression for the DC current vector should be
+    temp_i = 0
+    for j in range(0, 3):
+        for k in range(0, 3): #
+            vector_component_list[j] += sp.simplify(en[j] * en[k] * matrix[temp_i][j][k])
+            # Allows me to navigate
+            if j == 2:
+                temp_i += 1
+    print(vector_component_list)
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pyperclip as pc  # To copy and paste the final matrix when it has been processed
 import functions as fns  # Functions file to keep it more organised
+import sympy as sp
 
-# Change values of the 3x6 here
-threeBySix = [[0, 0, 0, 0, 'a', 0], [0, 0, 0, 'a', 0, 0], ['b', 'b', 'c', 0, 0, 0]]
+# Change values and of the 3x6 here
+a = sp.exp('a'); b = sp.exp('b'); c = sp.exp('c')
+threeBySix = [[1, 1, 1, 1, a, 1], [1, 1, 1, b, 1, 1], [b, b, c, 1, 1, 1]]
 
 # Placeholder values before changing them
 threeByThree = [[[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0],
 [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]]
+
 
 # Loop through each row of the 3x6 matrix
 for i in range(0, 3):
@@ -15,10 +18,14 @@ for i in range(0, 3):
         # Function to change the values accordingly
         fns.adjust_value(i, j, threeByThree, threeBySix)
 
+fns.calculate_dccurrent_vector(threeByThree)
+print(".")
+
 # Printing the original and final matrix
 print('Original Matrix: ' + threeBySix.__str__())
 print('Matrix as 3x3x3: ' + threeByThree.__str__())
 
+
 # Copy the new matrix to clipboard using pyperclip package
-pc.copy(threeByThree.__str__())
-print('3x3 Matrix Copied to Clipboard')
+# pc.copy(threeByThree.__str__())
+# print('3x3 Matrix Copied to Clipboard')
