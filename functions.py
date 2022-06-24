471a5d1020c2ab151529bfb7657f9ce7f5983219
Maxime Evrard <maximee2022@student.cis.edu.hk>
2022-06-24 15:20:41 -0700

    * Minor adjustments to formatting and comments, tried to check calculate dc current vector but couldn't

--- functions.py
+++ functions.py
@@ -1,4 +1,3 @@
-import numpy as np
 import sympy as sp  # Symbolic mathematics package
 
 # Adjust value will take in two indices (for the column and row of the 3x6 matrix)
@@ -28,18 +27,16 @@ def adjust_value(i_index, j_index, three_by_three, three_by_six):
 # Perform double sum
 
 
-def calculate_dccurrent_vector(matrix):
+def calculate_dc_current_vector(matrix):
     # Defining expressions before I can use them
     x = sp.symbols('x'); cosine = sp.cos(x); sine = sp.sin(x)
     # Defining the components of the magnetic field and final vector, respectively
     en = [cosine, sine, 1]
     vector_component_list = [0, 0, 0]
-    # Working out what the final expression for the DC current vector should be
     temp_i = 0
     for j in range(0, 3):
-        for k in range(0, 3): #
+        for k in range(0, 3):
             vector_component_list[j] += sp.simplify(en[j] * en[k] * matrix[temp_i][j][k])
-            # Allows me to navigate
             if j == 2:
                 temp_i += 1
     print(vector_component_list)
--- main.py
+++ main.py
@@ -2,14 +2,19 @@ import pyperclip as pc  # To copy and paste the final matrix when it has been pr
 import functions as fns  # Functions file to keep it more organised
 import sympy as sp
 
+"""
+* Check if the function made yesterday actually works by doing algebra to 
+compare values to dan's mathematica file (try to do this by doing the math to 
+untransformed tensor to see if it is the same)
+"""
+
 # Change values and of the 3x6 here
 a = sp.exp('a'); b = sp.exp('b'); c = sp.exp('c')
 threeBySix = [[1, 1, 1, 1, a, 1], [1, 1, 1, b, 1, 1], [b, b, c, 1, 1, 1]]
 
 # Placeholder values before changing them
 threeByThree = [[[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0],
-[0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]]
-
+                                                    [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]]]
 
 # Loop through each row of the 3x6 matrix
 for i in range(0, 3):
@@ -18,13 +23,12 @@ for i in range(0, 3):
         # Function to change the values accordingly
         fns.adjust_value(i, j, threeByThree, threeBySix)
 
-fns.calculate_dccurrent_vector(threeByThree)
+fns.calculate_dc_current_vector(threeByThree)
 print(".")
 
 # Printing the original and final matrix
 print('Original Matrix: ' + threeBySix.__str__())
 print('Matrix as 3x3x3: ' + threeByThree.__str__())
-
 
 # Copy the new matrix to clipboard using pyperclip package
 # pc.copy(threeByThree.__str__())
