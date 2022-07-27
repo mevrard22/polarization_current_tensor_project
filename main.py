4b041778a05dc43a3bacff55bd8265bf45e49175
Maxime Evrard <maximee2022@student.cis.edu.hk>
2022-07-27 13:40:15 -0700

    Added todos so when I come back i know what TODO

--- main.py
+++ main.py
@@ -12,3 +12,8 @@ number = int(input("How many tensors would you like to enter?"))
 # Loop through whatever that number so user can enter all tensors they need calculations for
 for number in range(0, number):
     l += [functions.enter_matrix()]
+
+# TODO 1: Graph the components of A tensors on a graph, with labels and a key
+
+
+# TODO 2: Graph the components of BOTH tensors on two seperate graphs, with labels and a key
