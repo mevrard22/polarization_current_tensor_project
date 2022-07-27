b94a6bf32e8e9162540ac6c2900b051ba8df8e39
Maxime Evrard <maximee2022@student.cis.edu.hk>
2022-07-27 13:35:47 -0700

    * Make it possible for the user to enter multiple tensors at a time from the console * Store the tensors within a list that works fine

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 import pyperclip as pc  # To copy and paste the final matrix when it has been processed
 
 import functions
-import functions as fns  # Functions file to keep it more organised
 import sympy as sp
 
-"""
-* Check if the function made yesterday actually works by doing algebra to 
-compare values to dan's mathematica file (try to do this by doing the math to 
-untransformed tensor to see if it is the same)
-"""
 
+# Define a list to put each tensor into
+l = []
 
-# Placeholder values before changing them
-# print(functions.enter_matrix())
+# Find the number of tensors the user would like to enter
+number = int(input("How many tensors would you like to enter?"))
 
-# fns.calculate_dc_current_vector(threeByThree)
-
-# Printing the original and final matrix
-# print('Original Matrix: ' + threeBySix.__str__())
-# print('Matrix as 3x3x3: ' + threeByThree.__str__())
-
-# Copy the new matrix to clipboard using pyperclip package
-# pc.copy(threeByThree.__str__())
-# print('3x3 Matrix Copied to Clipboard')
+# Loop through whatever that number so user can enter all tensors they need calculations for
+for number in range (0, number):
+    l += [functions.enter_matrix()]
